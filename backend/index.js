08ca63b1e5fae09f17b7729e2779e42122c1436d
RajKishor Kumar <rajkishorkumarjan04@gmail.com>
2026-01-17 12:00:24 +0530

    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://stockify-2.onrender.com","https://stockify-1-0w90.onrender.com"],
     credentials: true,
   })
 );
